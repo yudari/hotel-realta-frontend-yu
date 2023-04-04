1aa992c915cb5733dbca3e176924fd38c7db7760
Rifqi Pratama <rifqipratama9@gmail.com>
2023-04-04 16:04:03 +0700

    modified error page to the outside the layout (#9)
    * add input select
    
    * add modal
    
    * change layout
    
    * change primary color
    
    * change navbar dropdown logout
    
    * change sidebar layout and and add breadcrumbs component
    
    * mofified error page outside the layout page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import NextNProgress from "nextjs-progressbar";
 import Layout from "@/components/shared/Layout";
 import { Poppins } from "next/font/google";
+import { useRouter } from "next/router";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700", "800", "900"],
@@ -13,6 +14,14 @@ const poppins = Poppins({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const hasLayout =
+    !router.pathname.startsWith("/users/loginEmployee") &&
+    !router.pathname.startsWith("/users/signupEmployee") &&
+    !router.pathname.startsWith("/users/loginGuest") &&
+    !router.pathname.startsWith("/users/signupGuest") &&
+    router.pathname !== "/_error";
+
   return (
     <Provider store={store}>
       <style jsx global>
@@ -25,9 +34,13 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <NextNProgress color="black" />
 
-      <Layout>
+      {hasLayout ? (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      ) : (
         <Component {...pageProps} />
-      </Layout>
+      )}
     </Provider>
   );
 }
