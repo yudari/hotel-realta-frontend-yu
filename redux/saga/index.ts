434a60051122400e33f74264a633cf4619f3ede0
rarachaterine <rarachaterine03@yahoo.com>
2023-04-10 19:20:20 +0700

    change saga

--- redux/saga/index.ts
+++ redux/saga/index.ts
@@ -1,32 +1,10 @@
-import{ takeEvery, all} from 'redux-saga/effects';
-import actionType from '../payment/action/actionType';
-import { handleAddBank, handleDeleteBank, handleGetBank, handleUpdateBank } from '../payment/paymentSaga/bankSaga';
-import { handleAddFintech, handleDeleteFintech, handleGetFintech, handleUpdateFintech } from '../payment/paymentSaga/fintechSaga';
-import { handleGetPayTrans, handleTopup } from '../payment/paymentSaga/paymentTransactionSaga';
-import { handleDeleteUserAcc, handleGetBankFintech, handleGetUserAcc, handleUpdateUserAcc, hanleAddUserAcc } from '../payment/paymentSaga/userAccountSaga';
+import { all } from "redux-saga/effects";
 
-function* watchAll(){
-    yield all([
-        takeEvery(actionType.GET_BANK, handleGetBank),
-        takeEvery(actionType.ADD_BANK, handleAddBank),
-        takeEvery(actionType.UPDATE_BANK, handleUpdateBank),
-        takeEvery(actionType.DEL_BANK, handleDeleteBank),
+import paymentSaga from "../payment/paymentSaga/index";
 
-        takeEvery(actionType.GET_FINTECH, handleGetFintech),
-        takeEvery(actionType.ADD_FINTECH, handleAddFintech),
-        takeEvery(actionType.UPDATE_FINTECH, handleUpdateFintech),
-        takeEvery(actionType.DEL_FINTECH, handleDeleteFintech),
 
-        takeEvery(actionType.GET_PAYMENT_TRANSACTION, handleGetPayTrans),
-        takeEvery(actionType.POST_TOPUP, handleTopup),
-        
-
-        takeEvery(actionType.GET_USER_ACCOUNT, handleGetUserAcc),
-        takeEvery(actionType.ADD_USER_ACCOUNT, hanleAddUserAcc),
-        takeEvery(actionType.UPDATE_USER_ACCOUNT, handleUpdateUserAcc),
-        takeEvery(actionType.DEL_USER_ACCOUNT, handleDeleteUserAcc),
-        takeEvery(actionType.GET_BANK_FINTECH, handleGetBankFintech),
-    ])
+export default function* rootSaga() {
+  yield all([
+    paymentSaga()
+  ]);
 }
-
-export default watchAll
