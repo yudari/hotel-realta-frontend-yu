7d0a91520fa584688317cd80aca276936d3e3b3a
Rifqi Pratama <rifqipratama9@gmail.com>
2023-04-17 02:12:35 +0700

    fix: price items cancel button and edit (#36)
    * punya ikrar
    
    * add master ikrar
    
    * add new saga
    
    * change table master
    
    * fix: fix table header color
    
    * add cancel button in price items
    
    * hallo
    
    ---------
    
    Co-authored-by: ikrarilham <ikrarilham09@gmail.com>

--- pages/master/priceitems/addPriceItems.tsx
+++ pages/master/priceitems/addPriceItems.tsx
@@ -182,7 +182,9 @@ export default function AddPriceMaster(props: any) {
                         <button
                           className="inline-flex justify-center rounded-md border border-transparent bg-blug-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                           onClick={props.closeModal}
-                        />
+                        >
+                          Cancel
+                        </button>
                       </div>
                     </form>
                   </div>
